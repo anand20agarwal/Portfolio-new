74d543cdeb377dc4b329dbc5e6303dbd796cbb29
Anand Agarwal <anand20agarwal00@gmail.com>
2024-03-19 21:01:21 +0530

    Updates

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,8 +3,35 @@ import './Design.css'
 
 function Project() {
   return (
-    <div className='contain'>
-      Project Pagwe
+    <div className='contain-project'>
+      <br/>
+      <br/>
+     <hr/>
+     <h1>Projects</h1>
+     <hr/>
+     <div className='containers1'>
+      <h2 className='pname'> University Management System</h2>
+      <p className='para'>  A comprehensive software solution
+ facilitating efficient administration, student management, academic
+ processes, and resource coordination in educational institutions</p>
+ <br/> 
+ <h2 className='pname'> Book Store</h2>
+      <p className='para'>  A digital platform enabling users to browse, purchase,
+ and manage awide range of books, enhancing accessibility and
+ convenience in book shopping</p>
+ <br/>
+ <h2 className='pname'> Quiz App</h2>
+      <p className='para'>  A digital platform enabling users to engage in interactive
+ quizzes, offering diverse question sets, real-time feedback, and
+ customizable features for enhanced learning and entertainment.</p>
+<br/>
+<h2 className='pname'> Portfolio</h2>
+      <p className='para'>  Aportfolio project is a concise showcase of an individual's skills,
+ accomplishments, and completed works. It includes diverse samples, such
+ as websites, designs, or writing pieces, demonstrating technical expertise
+ and creativity</p>
+
+     </div>
     </div>
   )
 }
