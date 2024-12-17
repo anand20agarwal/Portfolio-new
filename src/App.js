d5a144bac92529202c30bd18c2419986c9663166
Anand Agarwal <anand20agarwal00@gmail.com>
2024-12-17 11:47:20 +0530

    Deployment Changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-// App.js
-
 import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
@@ -7,22 +5,21 @@ import About from './components/About';
 import Contact from './components/Contact';
 import Project from './components/Project';
 import Navbar from './components/Navbar';
-import IMG from './components/IMG.jpg'
-import './components/Design.css'
-
+import IMG from './components/IMG.jpg';
+import './components/Design.css';
 
 function App() {
   return (
     <HashRouter>
-    <div>
-      <Navbar />
-      <img src={IMG} className='image' alt='/' />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/project" element={<Project />} />
-      </Routes>
+      <div>
+        <Navbar />
+        <img src={IMG} className="image" alt="Portfolio Background" />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/project" element={<Project />} />
+        </Routes>
       </div>
     </HashRouter>
   );
