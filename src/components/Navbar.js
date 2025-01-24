1bc783af4602ce1f2eb1bbdc32108951fd6e136c
Anand Agarwal <anand20agarwal00@gmail.com>
2025-01-24 19:05:47 +0530

    Updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Navbar() {
   return (
     <nav className="navbar">
       <ul className="navbar-list">
-        <li className="navbar-item"><Link to="/" className="navbar-link">Home</Link></li>
-        <li className="navbar-item"><Link to="/about" className="navbar-link">About</Link></li>
-        <li className="navbar-item"><Link to="/project" className="navbar-link">Project</Link></li>
-        <li className="navbar-item"><Link to="/contact" className="navbar-link">Contact</Link></li>
+        <li className="navbar-item"><NavLink to="/#/" className="navbar-link">Home</NavLink></li>
+        <li className="navbar-item"><NavLink to="/#/about" className="navbar-link">About</NavLink></li>
+        <li className="navbar-item"><NavLink to="/#/project" className="navbar-link">Project</NavLink></li>
+        <li className="navbar-item"><NavLink to="/#/contact" className="navbar-link">Contact</NavLink></li>
       </ul>
     </nav>
   );
